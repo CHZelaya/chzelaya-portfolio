bf09a07022d5b310567656b18c143cab45963be7
CHZelaya <chzelaya1201@gmail.com>
2026-03-29 17:40:20 -0600

    fix: Adjusted the type schemas to make the About Section a singleton, updated imports in sanity.config.ts and resturctured structure.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -3,9 +3,9 @@ import { type SchemaTypeDefinition } from 'sanity'
 import { technologyType } from './technologyType'
 import { projectsType } from './projectsType'
 import { mediaType } from './mediaType'
-import { aboutType } from './aboutType'
+
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [aboutType, technologyType, projectsType, mediaType,],
+  types: [technologyType, projectsType, mediaType,],
 }
