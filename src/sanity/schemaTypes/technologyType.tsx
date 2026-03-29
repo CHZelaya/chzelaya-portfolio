bfbde5049c62bed3ac7658322771c30dac986569
CHZelaya <chzelaya1201@gmail.com>
2026-03-28 19:16:25 -0600

    feature/ technologyType schema complete

---
+++ src/sanity/schemaTypes/technologyType.tsx
@@ -0,0 +1,40 @@
+import { defineArrayMember, defineField, defineType } from "sanity";
+
+export const technologyType = defineType({
+    name: 'Technology',
+    title: 'Technology',
+    type: 'document',
+    fields: [
+        defineField({
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        }),
+        defineField({
+            name: 'category',
+            title: 'Category',
+            type: 'array',
+            of: [defineArrayMember({
+                type: 'string',
+                options: {
+                    list: ['Language',
+                        'Framework',
+                        'DevOps',
+                        'Database',
+                        'Devops',
+                        'Tool',
+
+                    ]
+                }
+            })],
+        }),
+        defineField({
+            name: 'icon',
+            title: 'Icon',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        })
+    ]
+})
