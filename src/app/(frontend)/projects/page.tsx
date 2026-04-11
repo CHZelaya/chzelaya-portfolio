ff809a834ca23d615f5d68dcb52ec9ef6d540913
CHZelaya <chzelaya1201@gmail.com>
2026-04-11 14:35:42 -0600

    Fix: Added an export default function declaration to projects/page.tsx

--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -1,1 +1,8 @@
 //TODO: This will be the projects library page, with filters and all projects. DevPanel will become a "featured projects" section on the homepage.
+
+
+export default function ProjectsPage() {
+    return (
+        console.log("Projects Page working")
+    )
+}
