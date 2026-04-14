3f1cf5b3564a33c2e7619188ee7925ef36402612
CHZelaya <chzelaya1201@gmail.com>
2026-04-14 10:19:17 -0600

    WIP: Refactor/Reshuffle of Components and directories needed, committing all work thus far before starting the refactor

--- src/app/(frontend)/projects/DevProfileClient.tsx
+++ src/app/(frontend)/projects/DevProfileClient.tsx
@@ -1,13 +1,19 @@
 import { PROJECTS_QUERYResult } from "@/sanity/types";
 import { DEV_PROFILE_QUERYResult } from "@/sanity/types";
+import { Marquee } from "@/components/ui/Marquee";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
 
 interface DevProfileClientProps {
     projects: PROJECTS_QUERYResult;
     devProfile: DEV_PROFILE_QUERYResult;
 }
 
+
 export default function DevProfileClient({ devProfile, projects }: DevProfileClientProps) {
-    console.log("DevProfileClient", { devProfile, projects });
+
+
+
     return (
         <div>
             <h1>Dev Profile</h1>
